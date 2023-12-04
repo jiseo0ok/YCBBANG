724a372e9aa46e7c30041295850764cdfec60845
jiseo0ok <129488360+jiseo0ok@users.noreply.github.com>
2023-12-05 06:25:05 +0900

    Add files via upload

--- login.php
+++ login.php
@@ -1,51 +1,55 @@
 <?php
-
 $uid = $_POST['uid'];
 $upass = $_POST['upass'];
 
+$servername = "realbread.csjj47wsn6pa.us-east-1.rds.amazonaws.com";
+$username = "root";
+$password = "rootroot";
+$dbname = "bread";
 
-$servername = "realbread.csjj47wsn6pa.us-east-1.rds.amazonaws.com"; // 데이터베이스 서버 이름
-		$username = "root"; // 데이터베이스 사용자명
-		$password = "rootroot"; // 데이터베이스 암호
-		$dbname = "bread"; // 사용할 데이터베이스 이름
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-		// MySQL 연결 생성
-		$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-$sql = "SELECT * FROM kbread WHERE upass = '{$upass}' AND uid = '{$uid}'";
-$result=mysqli_query($conn,$sql);
-$count = mysqli_num_rows($result);
+// Using prepared statements to prevent SQL injection
+$stmt = $conn->prepare("SELECT * FROM kbread WHERE uid = ? LIMIT 1");
+$stmt->bind_param("s", $uid);
+$stmt->execute();
+$result = $stmt->get_result();
 
-
-
-for($i=0; $i<$count; $i++){
- $re=mysqli_fetch_row($result);
-
-
-
-if ($re[1] === $uid && $re[4] === $upass) {
-    // 로그인 성공
-    // 세션에 id 저장
-    // session_start();
-    // $_SESSION['userId'];
-    // print_r($_SESSION);
-    // echo $_SESSION['userId'];
-    
+if ($result->num_rows === 1) {
+    $row = $result->fetch_assoc();
+    // Verify password using password_verify() function if passwords are hashed
+    if ($row['upass'] === $upass) {
+        // Start session and set user ID
+        // session_start();
+        // $_SESSION['userId'] = $row['uid'];
 ?>
-    <script>
-        alert("로그인에 성공하였습니다.")
-        location.href = "index1.php";
-    </script>
-
+        <script>
+            alert("로그인에 성공하였습니다.");
+            location.href = "index1.php";
+        </script>
 <?php
+    } else {
+?>
+        <script>
+            alert("로그인에 실패하였습니다.");
+            location.href = "login.html";
+        </script>
+<?php
+    }
 } else {
-    // 로그인 실패 
 ?>
     <script>
-        alert("로그인에 실패하였습니다.")
+        alert("사용자를 찾을 수 없습니다.");
         location.href = "login.html";
     </script>
 <?php
 }
-}
+
+$stmt->close();
+$conn->close();
 ?>
